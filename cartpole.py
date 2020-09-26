bb2c9818b395ea3c9bf1dd55a63ef51e3de132a0
atenagm <ashenatena@gmail.com>
2020-09-26 16:57:35 +0330

    Initialize cartpole script

---
+++ cartpole.py
@@ -0,0 +1,37 @@
+"""
+==============================================================================.
+
+cartpole.py
+
+@author: atenagm
+
+==============================================================================.
+"""
+from bindsnet.environment import GymEnvironment
+from bindsnet.learning.reward import MovingAvgRPE
+
+from agents import ObserverAgent
+
+
+def cartpole_observation_encoder(datum, time, dt=1.0, device="cpu"):
+    pass
+
+
+environment = GymEnvironment('CartPole-v1')
+environment.reset()
+
+observer = ObserverAgent(environment, dt=1.0, reward_fn=MovingAvgRPE)
+
+# environment_pipeline = EnvironmentPipeline(
+#     agent,
+#     environment,
+#     encoding=cartpole_observation_encoder,
+#     action_function=expert_agent_action,
+#     output="Output Layer",
+#     time=100,
+#     history_length=5,
+#     delta=1,
+#     plot_interval=1,
+#     render_interval=1,
+#     percent_of_random_action=0.01
+# )
