change output shape, fix softmax(decide based on firing rate), some parameter modifications

agents.py
@@ -119,35 +119,35 @@ class ObserverAgent(Agent):
         # TODO Consider network structure
         s2 = Input(shape=[*input_shape, 10], traces=True,
                    traces_additive=True,
-                   tc_trace=6.0,
+                   tc_trace=15.0,
                    trace_scale=0.8
                    )
-        pm = DiehlAndCookNodes(shape=[output_shape, 1], traces=True,
+        pm = DiehlAndCookNodes(shape=[output_shape, 10], traces=True,
                                traces_additive=True,
-                               tc_trace=6.0,
+                               tc_trace=15.0,
                                trace_scale=0.8,
-                               thresh=-54.0,
+                               thresh=-62.5,
                                rest=-65.0,
                                reset=-65.0,
                                refrac=5,
-                               tc_decay=4.0,
-                               theta_plus=2e-4,
-                               tc_theta_decay=5e6,
+                               tc_decay=15.0,
+                               theta_plus=0.,
+                               tc_theta_decay=5e7,
                                one_spike=True
                                )
 
         s2_pm = Connection(s2, pm,
-                           nu=0.05,
+                           nu=0.5,
                            update_rule=MSTDPET,
                            wmin=0.0,
                            wmax=1.0,
                            # norm=0.2 * s2.n,
-                           tc_plus=3.,
-                           tc_minus=4.,
-                           tc_e_trace=8.,
+                           tc_plus=15.,
+                           tc_minus=15.,
+                           tc_e_trace=60.,
                            )
         pm_pm = Connection(pm, pm,
-                           w=-5e-5 * torch.eye(pm.n)
+                           w=-1e-5 * torch.ones(pm.n)
                            )
 
         self.network.add_layer(s2, "S2")
@@ -190,11 +190,12 @@ class ObserverAgent(Agent):
                 return spikes[0, 1]
 
         # Select action using softmax.
-        # TODO fix bug
         if self.method == 'softmax':
-            spikes = torch.sum(spikes, dim=0)
+            spikes = torch.sum(spikes, dim=0).squeeze(0)
+            spikes = torch.sum(spikes, dim=1)
             probs = torch.softmax(spikes, dim=0)
-            return torch.multinomial(probs, num_samples=1).item()
+            most_fired = torch.argmax(probs)
+            return most_fired
 
         # Select action randomly.
         if self.method == 'random' or self.method is None: