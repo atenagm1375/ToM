413b5afdc3c1dddc7e56334a6f1fae669e5d7df2
atenagm <ashenatena@gmail.com>
2020-11-21 13:05:17 +0330

    remove passing n_neurons

--- cartpole.py
+++ cartpole.py
@@ -178,7 +178,7 @@ w = pipeline.network.connections[("S2", "PM")].w
 # plot_weights(w)
 print(w)
 
-pipeline.train_by_observation(weight='/home/atenagm/hill_climbing.pt', n_neurons=10)
+pipeline.train_by_observation(weight='/home/atenagm/hill_climbing.pt')
 print("Observation Finished")
 
 w = pipeline.network.connections[("S2", "PM")].w
@@ -186,7 +186,7 @@ w = pipeline.network.connections[("S2", "PM")].w
 print(w)
 
 for i in range(100):
-    pipeline.test(n_neurons=10)
+    pipeline.test()
 
 test_rewards = pipeline.reward_list[-100:]
 print("min:", np.min(test_rewards), "max:", np.max(test_rewards), "average:",
