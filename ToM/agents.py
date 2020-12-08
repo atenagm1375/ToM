58f30f7be581dcbdd38356360a4037cf890bf39c
atenagm <ashenatena@gmail.com>
2020-12-08 19:20:49 +0330

    add Zaxxon agent network(the one to be trained as expert)

--- ToM/agents.py
+++ ToM/agents.py
@@ -274,6 +274,114 @@ class CartPoleObserverAgent(ObserverAgent):
         self.network.to(self.device)
 
 
+class ZaxxonAgent(ObserverAgent):
+    """
+    A agent in Gym Zaxxon environment.
+
+    Parameters
+    ----------
+    environment : GymEnvironment
+        The environment of the observer agent.
+    method : str, optional
+        The method that the agent acts upon. Possible values are:
+            `first_spike`: Select the action with the first spike.
+            `softmax`: Select an action using softmax function on the spikes.
+            `random`: Select actions randomly.
+        The default is 'fist_spike'.
+    dt : float, optional
+        Network simulation timestep. The default is 1.0.
+    learning : bool, optional
+        Whether to allow network connection updates. The default is True.
+    reward_fn : AbstractReward, optional
+        Optional class allowing for modification of reward in case of
+        reward-modulated learning. The default is None.
+    allow_gpu : bool, optional
+        Allows automatic transfer to the GPU. The default is True.
+
+    """
+
+    def __init__(
+            self,
+            environment: GymEnvironment,
+            method: str = 'first_spike',
+            dt: float = 1.0,
+            learning: bool = True,
+            reward_fn: AbstractReward = None,
+            allow_gpu: bool = True,
+    ) -> None:
+
+        super().__init__(environment, method, dt, learning, reward_fn, allow_gpu)
+
+    def build_network(self) -> None:
+        """
+        Build network for the agent.
+
+        Returns
+        -------
+        None
+
+        """
+        # Input layer
+        s2 = Input(shape=[2, 128], traces=True,
+                   traces_additive=True,
+                   tc_trace=1.0,
+                   trace_scale=0.0
+                   )
+        # Output layer
+        out_shape = self.environment.env.action_space.n
+        pm = DiehlAndCookNodes(shape=[out_shape, 1], traces=True,
+                               traces_additive=True,
+                               tc_trace=1.0,
+                               trace_scale=0.0,
+                               thresh=-64.,
+                               rest=-65.0,
+                               reset=-65.0,
+                               refrac=20,
+                               tc_decay=2.0,
+                               theta_plus=0.0,
+                               tc_theta_decay=1e6,
+                               one_spike=True
+                               )
+        # Initial weights
+        w1 = torch.normal(torch.zeros(s2.n, pm.n), 0.01 * torch.ones(s2.n, pm.n))
+
+        # Input to output connections which will be trained using R-STDP
+        s2_pm = Connection(s2, pm,
+                           nu=0.2,
+                           update_rule=MSTDPET,
+                           wmin=-1.0,
+                           wmax=1.0,
+                           w=w1,
+                           tc_plus=7.,
+                           tc_minus=7.,
+                           tc_e_trace=180.,
+                           # weight_decay=1e-6,
+                           )
+        # Inhibitory connection on output neurons to regulate their activation
+        pm_pm = Connection(pm, pm,
+                           w=-0.5 * (torch.ones(pm.n))
+                           )
+
+        # Add layers/populations to the network
+        self.network.add_layer(s2, "S2")
+        self.network.add_layer(pm, "PM")
+        # Add connections to the network
+        self.network.add_connection(s2_pm, "S2", "PM")
+        self.network.add_connection(pm_pm, "PM", "PM")
+        # Add monitors
+        self.network.add_monitor(
+            Monitor(self.network.layers["PM"], ["s", "v"]), "PM",
+        )
+        self.network.add_monitor(
+            Monitor(self.network.layers["S2"], ["s"]), "S2"
+        )
+        self.network.add_monitor(
+            Monitor(self.network.connections[("S2", "PM")], ["w"]), "S2-PM"
+        )
+
+        self.network.to(self.device)
+
+
 class ExpertAgent(Agent):
     """
     Expert agent in Gym environment.
